fedca9a9a25ab9b226b80f7579862aa6c1d569c1
Sébastien Huss <sebastien.huss@gmail.com>
2017-10-22 10:58:58 +0200

    Fixed bs.box collapsible behavior

--- src/d3-bootstrap-extra.js
+++ src/d3-bootstrap-extra.js
@@ -42,7 +42,7 @@ bs.box	= function() {
 				}
 				var btn = d3.select(this).append('button').attr('type', 'button').attr('class','btn btn-box-tool');
 				if (typeof d.action != 'undefined' && d.action == 'collapse')
-					btn.attr('data-widget', d.action).attr('data-target','#'+id);
+					btn.attr('data-widget', d.action).attr('data-target','#'+id).on('click.bs.collapse.data-api',	bs.api.collapse.click);
 				else if (typeof d.action != 'undefined')
 					btn.attr('data-widget', d.action)
 				if (typeof d.icon != 'undefined')
